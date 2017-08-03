98fe11c76ab5dd59f5335abfe010725f7fabc093
jakedub <jacob.w.moore@gmail.com>
2017-08-03 14:59:13 -0400

    additional

--- scripts/main.js
+++ scripts/main.js
@@ -8,60 +8,65 @@
    you'll need to parse through that first before you can start to
    write your logic.
 */
-const deck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J","Q","K","A"];
-let pairs = [];
-let hand = 0;
+const hand = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J","Q","K","A"];
+
 function handValue (hand) {
- let myHandValue = 0;
- let newCard;
- for (let i = 0; i < hand.length; i++) {
-   if (hand[i] === "J" || hand[i] === "Q" || hand[i] === "K");
-   myHandValue += 10;
- } else if (hand[i] === "2" || hand[i] === "3" || hand[i] === "4" || hand[i] === "5" || hand[i] === "6" || hand[i] === "7" || hand[i] === "8" || hand[i] ==="9")
- {
-   switch (hand[i]) {
-     case "2":
-       myHandValue = 2;
-       break;
-     case "3":
-       myHandValue = 3
-       break;
-     case "4":
-        myHandValue =4
+  let myHandValue = 0;
+  moveAces(hand);
+  for (let i=0; i<hand.length; i++) {
+    if (hand[i] === "J" || hand[i] === "Q" || hand[i] === "K") {
+      myHandValue += 10;
+    } else if (hand[i] === "2" || hand[i] === "3" || hand[i] === "4" || hand[i] === "5" || hand[i] === "6" || hand[i] === "7"|| hand[i] === "8" || hand[i] === "9" || hand[i] === "10") {
+    switch (hand[i]) { //posible to change this to myHandValue += Number(hand[i]) right?
+      case "2":
+        myHandValue += 2;
         break;
-       case "5":
-       myHandValue =5
-        break;
-       case "6":
-         myHandValue = 6
-         break;
-       case "7":
-         myHandValue =7
-         break;
-       case "8":
-         myHandValue =8
-         break;
-       case "9":
-         myHandValue =9
-         break;
-       case "10":
-         myHandValue =10
-         break;
-   }
-   }
- }
+        case "3":
+          myHandValue += 3;
+          break;
+        case "4":
+          myHandValue += 4;
+          break;
+        case "5":
+          myHandValue += 5;
+          break;
+        case "6":
+          myHandValue += 6 ;
+          break;
+        case "7":
+          myHandValue += 7;
+          break;
+        case "8":
+          myHandValue += 8;
+          break;
+        case "9":
+          myHandValue += 9;
+          break;
+        case "10":
+          myHandValue += 10;
+          break;
+      default:
+    }
+  } else if (hand[i] === "A") {
+    if (myHandValue <=10){
+      myHandValue += 11;
+    } else {
+      myHandValue += 1;
+    }
+  }
+  }
+  return myHandValue;
 }
 console.log(handValue());
 
-function moveAces(){
-  if (hand === "A"){
-    return 1;
-  } else {
-    return 11;
+function moveAces(hand) {
+  for (let j=0; j<hand.length; j++) {
+    if (hand[j] === "A"){
+      hand.splice(hand[j], 1);
+      hand.push("A");
+    }
   }
 }
-console.log(moveAces());
-
 
 
 /* -----  Hints ------
